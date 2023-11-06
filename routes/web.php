1c130a31dd65d546f6d3eafb0acadf62187d2502
ErnieMcFalls <82377263+ErnieMcFalls@users.noreply.github.com>
2023-11-06 16:41:02 +0700

    added Admin, Book and Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\AdminController; 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.home');
+    Route::get('/book', [BookController::class, 'index'])->name('admin.book');
+    Route::get('/category', [CategoryController::class, 'index'])->name('admin.category');
 });
